d56486c438dde01dc99fab2561d293fda8306a23
KriptoBaba382 <199w29c@mail.ru>
2025-04-09 14:18:40 +0300

    Add: is open menu and close menu

--- js/main.js
+++ js/main.js
@@ -3,16 +3,37 @@ const logo = document.querySelector(".logo-svg use")
 const mMenuToggle = document.querySelector(".mobile-menu-toggle")
 const menu = document.querySelector(".mobile-menu")
 
-window.addEventListener("scroll", () => {
-if(this.scrollY >1 ) {
+function lightModeOn() {
     navbar.classList.add("navbar-light")
     logo.href.baseVal = "img/icons.svg#logo-black";
-} else {
+}
+function lightModeOff() {
     navbar.classList.remove("navbar-light")
     logo.href.baseVal = "img/icons.svg#logo-light";
 }
+
+function openMenu() { 
+    menu.classList.add("is-open"); // вешает класс is-open
+    mMenuToggle.classList.add("close-menu");
+    document.body.style.overflow = "hidden"; //запрещаем прокрутку сайта под меню
+    lightModeOn();
+}
+function closeMenu() { //функция закрывания меню/
+    menu.classList.remove("is-open"); //убирает класс is-open
+    mMenuToggle.classList.remove("close-menu");
+    document.body.style.overflow = ""; // возвращает прокрутку сайта под меню
+    lightModeOff();
+}
+window.addEventListener("scroll", () => {
+this.scrollY >1 ? lightModeOn () : lightModeOff ();
+
 })
 mMenuToggle.addEventListener("click", (event) => {
-    // event.preventDefault();
-    menu.classList.toggle("is-open");
+    event.preventDefault();
+
+    if (menu.classList.contains("is-open")) { //if - если
+        closeMenu()
+    } else { // else - иначе
+        openMenu();
+    }
 })
