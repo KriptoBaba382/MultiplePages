f0d3227f18d84d16e3eb81ff749183464e679c84
KriptoBaba382 <199w29c@mail.ru>
2025-04-13 13:10:30 +0300

    Add: sliders

--- js/main.js
+++ js/main.js
@@ -37,3 +37,63 @@ mMenuToggle.addEventListener("click", (event) => {
         openMenu();
     }
 })
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    autoHeight: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+          ccenteredSlides: true,
+          slidesOffsetBefore: 50,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        }
+      }
+  });
+
+  
+  const swiper2 = new Swiper('.swiper-two', {
+    speed: 400,
+    autoHeight: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+          ccenteredSlides: true,
+          slidesOffsetBefore: 50,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        }
+      }
+      
+  });
+  
